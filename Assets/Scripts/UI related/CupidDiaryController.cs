6f9200ac40498ab52fecbefb998810777d55335a
Renan <renanssba@gmail.com>
2020-06-04 04:06:30 -0300

    small improvements to cupid diary

--- Assets/Scripts/UI related/CupidDiaryController.cs
+++ Assets/Scripts/UI related/CupidDiaryController.cs
@@ -1,48 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-
-[System.Serializable]
-public class DiaryEntry {
-  public string entryName;
-  public TextMeshProUGUI title;
-  public TextMeshProUGUI description;
-  public Image[] personImages;
-  public GameObject[] babiesIcons;
-
-
-  public void Initialize(Relationship r) {
-    int babiesCount = Relationship.childrenNumber[r.id];
-
-    personImages[0].sprite = ResourcesManager.instance.GetCharacterSprite(r.GetBoy().id, CharacterSpritePart.body);
-    personImages[1].sprite = ResourcesManager.instance.GetCharacterSprite(r.GetBoy().id, CharacterSpritePart.casual);
-    personImages[2].sprite = ResourcesManager.instance.GetCharacterSprite(r.GetGirl().id, CharacterSpritePart.body);
-    personImages[3].sprite = ResourcesManager.instance.GetCharacterSprite(r.GetGirl().id, CharacterSpritePart.casual);
-
-    for(int i=0; i<babiesIcons.Length; i++) {
-      if(i < babiesCount) {
-        babiesIcons[i].SetActive(true);
-      } else {
-        babiesIcons[i].SetActive(false);
-      }
-    }
-    GlobalData.instance.currentRelationshipId = r.id;
-    title.text = SpecialCodes.InterpretStrings(Lean.Localization.LeanLocalization.GetTranslationText("char_name/couple"));
-    description.text = SpecialCodes.InterpretStrings(Lean.Localization.LeanLocalization.GetTranslationText("couple_epilogue/"+r.GetGirl().nameKey));
-  }
-}
 
 
 public class CupidDiaryController : MonoBehaviour {
 
-  public DiaryEntry[] entries;
+  public static CupidDiaryController instance;
+
+  public CupidDiaryEntry[] entries;
   public int currentPage = 0;
+  public ScreenTransitions panel;
+
+
+  public void Awake() {
+    instance = this;
+    gameObject.SetActive(false);
+  }
 
 
   public void OnEnable() {
     entries[0].Initialize(GlobalData.instance.relationships[0]);
-    entries[1].Initialize(GlobalData.instance.relationships[1]);
+    entries[1].Initialize(GlobalData.instance.relationships[2]);
   }
 }
---
+++ Assets/Scripts/UI related/CupidDiaryEntry.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class CupidDiaryEntry : MonoBehaviour {
+  public TextMeshProUGUI title;
+  public TextMeshProUGUI description;
+  public Image[] personImages;
+  public GameObject[] babiesIcons;
+
+
+  public void Initialize(Relationship r) {
+    int babiesCount = Relationship.childrenNumber[r.id];
+
+    personImages[0].sprite = ResourcesManager.instance.GetCharacterSprite(r.GetBoy().id, CharacterSpritePart.body);
+    personImages[1].sprite = ResourcesManager.instance.GetCharacterSprite(r.GetBoy().id, CharacterSpritePart.casual);
+    personImages[2].sprite = ResourcesManager.instance.GetCharacterSprite(r.GetGirl().id, CharacterSpritePart.body);
+    personImages[3].sprite = ResourcesManager.instance.GetCharacterSprite(r.GetGirl().id, CharacterSpritePart.casual);
+
+    for(int i = 0; i < babiesIcons.Length; i++) {
+      if(i < babiesCount) {
+        babiesIcons[i].SetActive(true);
+      } else {
+        babiesIcons[i].SetActive(false);
+      }
+    }
+    GlobalData.instance.currentRelationshipId = r.id;
+    title.text = SpecialCodes.InterpretStrings(Lean.Localization.LeanLocalization.GetTranslationText("char_name/couple"));
+    description.text = SpecialCodes.InterpretStrings(Lean.Localization.LeanLocalization.GetTranslationText("couple_epilogue/" + r.GetGirl().nameKey));
+  }
+}
