7592c6ac2dafae19a60b8081030fc180bef487eb
luanlucas <luanolavolucas@gmail.com>
2020-06-06 17:06:31 -0300

    Added commands for playing and stopping ambience sfx, polished party scene a little

---
+++ Assets/VSN/Scripts/Audio Subsystem/PlayAmbienceCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Command
+{
+
+  [CommandAttribute(CommandString = "play_ambience")]
+  public class PlayAmbienceCommand : VsnCommand
+  {
+
+    public override void Execute() {
+        VsnAudioManager.instance.PlayAmbience(args[0].GetStringValue());
+    }
+
+    public override void AddSupportedSignatures() {
+      signatures.Add(new VsnArgType[] {
+        VsnArgType.stringArg
+      });
+    }
+  }
+}
---
+++ Assets/VSN/Scripts/Audio Subsystem/StopAmbienceCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Command
+{
+
+  [CommandAttribute(CommandString = "stop_ambience")]
+  public class StopAmbienceCommand : VsnCommand
+  {
+
+    public override void Execute() {
+        VsnAudioManager.instance.StopAmbience(args[0].GetStringValue());
+    }
+
+    public override void AddSupportedSignatures() {
+      signatures.Add(new VsnArgType[] {
+        VsnArgType.stringArg
+      });
+    }
+  }
+}
