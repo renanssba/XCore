2b7758bdd34ba83ea47b0d0e44b443c1b0609a21
Renan <renanssba@gmail.com>
2020-07-13 22:02:55 -0300

    small graphics changes

--- Assets/Editor/PreProcessTexturesToUI.cs
+++ Assets/Editor/PreProcessTexturesToUI.cs
@@ -5,13 +5,13 @@ class PreProcessTexturesToUI : AssetPostprocessor {
 
   void OnPreprocessTexture () {
     //return;
-		TextureImporter textureImporter = (TextureImporter)assetImporter;
+		//TextureImporter textureImporter = (TextureImporter)assetImporter;
 
-    if(textureImporter.assetPath.Contains("Resources")) {
-      textureImporter.textureType = TextureImporterType.Sprite;
-    }
-    if(textureImporter.assetPath.Contains("UI")){
-      textureImporter.textureType = TextureImporterType.Sprite;
-    }
+  //  if(textureImporter.assetPath.Contains("Resources")) {
+  //    textureImporter.textureType = TextureImporterType.Sprite;
+  //  }
+  //  if(textureImporter.assetPath.Contains("UI")){
+  //    textureImporter.textureType = TextureImporterType.Sprite;
+  //  }
 	}
 }
